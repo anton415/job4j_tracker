4c6ee4eeeba35f9112a9727eb8d782a0bdee6031
Anton Serdyuchenko <anton415@gmail.com>
2025-09-20 18:15:19 +0300

    Добавлены основные методы для работы с заявками.

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,32 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
+/**
+ * The Item class describes a statement model.
+ * @author Anton Serdyuchenko
+ * @since 16.09.2025
+ */
 public class Item {
+    /**
+     * The id field is a unique application number.
+     */
     private int id;
+    /**
+     * The name field contains the name of the application.
+     */
     private String name;
+    /**
+     * The date and time of created item.
+     */
+    private final LocalDateTime created = LocalDateTime.now();
+
+    public Item() {
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
 
     public int getId() {
         return id;
@@ -18,5 +42,9 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 }
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,16 +1,33 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
+/**
+ * Main class for working with Items.
+ * @author Anton Serdyuchenko
+ * @since 16.09.2025
+ */
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
     private int size = 0;
 
+    /**
+     * Add new item.
+     * @param item  - item.
+     * @return      - created item with id.
+     */
     public Item add(Item item) {
         item.setId(ids++);
         items[size++] = item;
         return item;
     }
 
+    /**
+     * Find item by id.
+     * @param id    - id of item.
+     * @return      - item.
+     */
     public Item findById(int id) {
         Item rsl = null;
         for (int index = 0; index < size; index++) {
@@ -21,5 +38,29 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    /**
+     * Get array of all items.
+     * @return  - items.
+     */
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
+    }
+
+    /**
+     * Get array of items by name.
+     * @param key   - name of item.
+     * @return      - array of items.
+     */
+    public Item[] findByName(String key) {
+        Item[] rsl = new Item[size];
+        int rslIndex = 0;
+        for (int index = 0; index < size; index++) {
+            if (key.equals(items[index].getName())) {
+                rsl[rslIndex++] = items[index];
+            }
+        }
+        return Arrays.copyOf(rsl, rslIndex);
     }
 }
