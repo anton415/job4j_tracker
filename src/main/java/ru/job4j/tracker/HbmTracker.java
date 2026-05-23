e511a05d7f684fe5a7949eb19cf67bb89c26087f
Anton Serdyuchenko <anton415@gmail.com>
2026-05-23 11:43:25 +0300

    3. HbmTracker [#287705 #564066]

--- src/main/java/ru/job4j/tracker/HbmTracker.java
+++ src/main/java/ru/job4j/tracker/HbmTracker.java
@@ -1,11 +1,14 @@
 package ru.job4j.tracker;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 import java.util.List;
+import java.util.function.Function;
 
 public class HbmTracker implements Store, AutoCloseable {
     private final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
@@ -15,36 +18,72 @@ public class HbmTracker implements Store, AutoCloseable {
 
     @Override
     public Item add(Item item) {
-        return null;
+        tx(session -> session.save(item));
+        return item;
     }
 
     @Override
     public boolean replace(Integer id, Item item) {
-        return false;
+        boolean rsl = tx(session -> session.createQuery(
+                        "UPDATE Item SET name = :name, created = :created WHERE id = :id"
+                )
+                .setParameter("name", item.getName())
+                .setParameter("created", item.getCreated())
+                .setParameter("id", id)
+                .executeUpdate() > 0);
+        if (rsl) {
+            item.setId(id);
+        }
+        return rsl;
     }
 
     @Override
     public boolean delete(Integer id) {
-        return false;
+        return tx(session -> session.createQuery("DELETE FROM Item WHERE id = :id")
+                .setParameter("id", id)
+                .executeUpdate() > 0);
     }
 
     @Override
     public List<Item> findAll() {
-        return null;
+        return tx(session -> session.createQuery(
+                "FROM Item ORDER BY id", Item.class
+        ).list());
     }
 
     @Override
     public List<Item> findByName(String key) {
-        return null;
+        return tx(session -> session.createQuery(
+                        "FROM Item WHERE name = :key ORDER BY id", Item.class
+                )
+                .setParameter("key", key)
+                .list());
     }
 
     @Override
     public Item findById(Integer id) {
-        return null;
+        return tx(session -> session.get(Item.class, id));
     }
 
     @Override
     public void close() {
         StandardServiceRegistryBuilder.destroy(registry);
+    }
+
+    private <T> T tx(Function<Session, T> command) {
+        Session session = sf.openSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            T rsl = command.apply(session);
+            transaction.commit();
+            return rsl;
+        } catch (RuntimeException e) {
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+            throw e;
+        } finally {
+            session.close();
+        }
     }
 }
